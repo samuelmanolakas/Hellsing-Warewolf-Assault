44955a6ad07a00c3c3edb24e03f7d8e91de09b9a
Samuel <samimanolakas@gmail.com>
2022-01-18 16:05:26 +0100

    Player detection zone

--- Ai.gd
+++ Ai.gd
@@ -12,12 +12,16 @@ onready var player_detection_zone = $PlayerDetectionZone
 var current_state: int = State.PATROl setget set_state
 onready var player = get_node("res://Player.tscn")
 
+
 func set_state(new_state: int):
 	if new_state == current_state:
 		return
 	
 	current_state = new_state
 	emit_signal("state_changed", current_state)
-	
+
+
 func _on_PlayerDetectionZone_body_entered(body: Node) -> void:
-	pass
+	if body.is_in_group("player"):
+		set_state(State.ENGAGE)
+		player = body
